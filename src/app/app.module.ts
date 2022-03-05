2f0b7c2c7a475579d140dd337b0dd4d63a0ae985
Gabriel Zapata <sr.gabrielzm@gmail.com>
2022-03-04 22:18:02 -0500

    Commit inicial del proyecto; creación de estructura de archivos, adición de las rutas de los diferentes componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { PaisModule } from './pais/pais.module';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CommonModule,
+
+    AppRoutingModule,
+
+    SharedModule,
+    PaisModule
+
+
   ],
   providers: [],
   bootstrap: [AppComponent]

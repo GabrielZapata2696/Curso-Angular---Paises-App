2c8cafeafb6db72b125272b7e3134401455f3856
Gabriel Zapata <sr.gabrielzm@gmail.com>
2022-03-04 23:19:12 -0500

    Maqueta HTML del componente por-Pais

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PorPaisComponent implements OnInit {
 
+  filtro: string = '';
+
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  buscar() {
+    console.log(this.filtro)
   }
 
 }
